4ef7c63e7e7a0f780b9506da6138c5a5f3926587
Andrew Fullerton <afully00@gmail.com>
2024-12-02 22:53:10 -0800

    added plot customizing arguments and unit tests to inspect_balance

--- R/inspect_balance.R
+++ R/inspect_balance.R
@@ -5,6 +5,8 @@
 #'
 #' @param data A date frame or tibble with at least one categorical variable.
 #' @param vars A vector of categorical variables contained in data. Default value `NULL` will produce plots for every categorical variable in the data.
+#' @param fill_colour An R-supported colour or hex code to assign as the barplot colour. Default value is "grey30"
+#' @param title A string. Default value `NULL` is no title.
 #' @param ... Additional ggplot2 parameters to modify plot outputs.
 #'
 #' @return A ggplot2 object with bar plots for each categorical variable.
@@ -20,8 +22,15 @@
 #'
 #' # Advanced usage
 #' # Manually specifies the variable to inspect and modifies plot output
-#' inspect_balance(iris, c("Species"), alpha = 0.5)
-inspect_balance <- function(data, vars = NULL, ...) {
+#' inspect_balance(data = iris,
+#'                 c("Species"),
+#'                 fill_colour = "blue",
+#'                 title = "Class balance of Species")
+inspect_balance <- function(data,
+                            vars = NULL,
+                            fill_colour = "grey30",
+                            title = NULL,
+                            ...) {
   # Check if input is a data frame
   if (!is.data.frame(data)) {
     stop("Input is not a data frame or a tibble.")
@@ -54,6 +63,18 @@ inspect_balance <- function(data, vars = NULL, ...) {
     stop("The following variables are not categorical: ", paste(non_categorical_vars, collapse = ", "))
   }
 
+  # Check if fill colour input is valid
+  valid_colors <- grDevices::colors()
+  is_hex_color <- grepl("^#(?:[0-9a-fA-F]{3}){1,2}$", fill_colour)
+  if (!(fill_colour %in% valid_colors || is_hex_color)) {
+    stop("Invalid color: ", fill_colour, ". Please provide a valid colour name or hex code.")
+  }
+
+  # Check if title provided is valid
+  if (!is.null(title) && (!is.character(title) || length(title) != 1 || title == "")) {
+    stop("Invalid title: ", title, ". Please provide a non-empty character string.")
+  }
+
   # Issue warning if selected variables exceed 20
   if (length(vars) > 20) {
     warning("The dataset has more than 20 selected variables. The plot might be crowded.")
@@ -63,20 +84,15 @@ inspect_balance <- function(data, vars = NULL, ...) {
   plot_balance <- data |>
     dplyr::select(dplyr::all_of(vars)) |>
     tidyr::pivot_longer(dplyr::everything(), names_to = "variable", values_to = "value") |>
-    ggplot2::ggplot(aes(x = value, fill = variable)) +
-      ggplot2::geom_bar(show.legend = FALSE, ...) +
-      ggplot2::facet_wrap(~ variable, scales = "free_x", strip.position = "top") +
-      ggplot2::theme_minimal() +
-      ggplot2::theme(
-        strip.background = element_blank(),
-        strip.placement = "outside",
-        axis.text.x = element_text(angle = 45, hjust = 1)
-      ) +
-      ggplot2::labs(
-        title = "Class Imbalance in Categorical Variables",
-        x = "Category",
-        y = "Count"
-      )
+    ggplot2::ggplot(aes(x = value)) +
+    ggplot2::geom_bar(show.legend = FALSE, fill = fill_colour, ...) +
+    ggplot2::facet_wrap(~ variable, scales = "free_x", strip.position = "top") +
+    ggplot2::theme(
+      strip.background = element_blank(),
+      strip.placement = "outside",
+      axis.text.x = element_text(angle = 45, hjust = 1)
+    ) +
+    ggplot2::labs(title = title)
 
   return(plot_balance)
 }
