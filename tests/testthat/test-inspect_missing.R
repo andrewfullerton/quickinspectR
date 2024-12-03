9edd766d24e72757c7e6a48eb21faea368c5a1de
Andrew Fullerton <afully00@gmail.com>
2024-12-02 23:25:11 -0800

    added plot customizing arguments to inspect_missing and added unit tests

--- tests/testthat/test-inspect_missing.R
+++ tests/testthat/test-inspect_missing.R
@@ -10,6 +10,28 @@ test_that("inspect_missing handles inputs as intended", {
   expect_error(inspect_missing(df_empty), "The dataset is empty. No plots will be generated.")
   expect_error(inspect_missing(df, vars = c("Sepal.Length", "nonexistent_var")),
                "The following variables do not exist in the dataset: nonexistent_var")
+
+  # Test colour and title errors and warnings
+  expect_error(
+    inspect_missing(iris, fill_colour = "invalid_color"),
+    "Invalid colour: invalid_color\\. Please provide a valid colour name or hex code\\."
+  )
+  expect_error(
+    inspect_missing(iris, fill_colour = "#GGGGGG"),
+    "Invalid colour: #GGGGGG\\. Please provide a valid colour name or hex code\\."
+  )
+  expect_error(
+    inspect_missing(iris, title = 123),
+    "Invalid title: 123\\. Please provide a non-empty character string\\."
+  )
+  expect_error(
+    inspect_missing(iris, title = ""),
+    "Invalid title: \\. Please provide a non-empty character string\\."
+  )
+  expect_error(
+    inspect_missing(iris, title = c("Title1", "Title2")),
+    "Invalid title: Title1Title2\\. Please provide a non-empty character string\\."
+  )
 })
 
 # ROUTINE USAGE TESTS
@@ -33,7 +55,11 @@ test_that("Advanced usage of inspect_missing produces ggplot object as intended"
   df[1:10, "Sepal.Length"] <- NA
 
   # Generate plot objects for testing
-  test_plot <- inspect_missing(df, vars = c("Sepal.Length"), na_colour = "blue", fill_colour = "green", alpha = 0.5)
+  test_plot <- inspect_missing(df,
+                               vars = c("Sepal.Length"),
+                               na_colour = "blue",
+                               fill_colour = "green",
+                               title = "Custom Title")
   barplot_layer <- test_plot$layers[[1]]
   test_plot_data <- ggplot2::ggplot_build(test_plot)$data[[1]]
   missing_fill_color <- unique(test_plot_data$fill[test_plot_data$fill == "blue"])
@@ -41,13 +67,16 @@ test_that("Advanced usage of inspect_missing produces ggplot object as intended"
   missing_alpha <- unique(test_plot_data$alpha[test_plot_data$fill == "blue"])
   non_missing_alpha <- unique(test_plot_data$alpha[test_plot_data$fill == "green"])
 
-  # Test plot output
+  # Test basic plot output
   expect_s3_class(test_plot, "ggplot")
   expect_s3_class(barplot_layer$geom, "GeomBar")
+
+  # Test if custom fill colour is correctly applied
   expect_equal(missing_fill_color, "blue")  # Missing values should be filled with blue
   expect_equal(non_missing_fill_color, "green")  # Non-missing values should be filled with green
-  expect_equal(missing_alpha, 0.5)  # Missing values should have an alpha of 0.5
-  expect_equal(non_missing_alpha, 0.5)  # Non-missing values should have an alpha of 0.5
+
+  # Check if custom title is set correctly
+  expect_equal(test_plot$labels$title, "Custom Title")
 })
 
 # EDGE CASE TESTS
@@ -66,7 +95,7 @@ test_that("Plot works when there are no missing values", {
   test_plot <- inspect_missing(df)
   test_plot_data <- ggplot2::ggplot_build(test_plot)$data[[1]] # Inspect plot build
   non_missing_data <- test_plot_data |>
-    dplyr::filter(fill == "darkgrey") # Assuming "darkgrey" represents non-missing
+    dplyr::filter(fill == "grey30") # Assuming "darkgrey" represents non-missing
   total_non_missing <- sum(non_missing_data$y) # Sum the y values to get non-missing value count
 
   # Test plot output
