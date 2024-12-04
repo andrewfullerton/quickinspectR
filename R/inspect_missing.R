faac5d3b49197596e387ce3021a42341e49a9889
Andrew Fullerton <afully00@gmail.com>
2024-12-03 22:26:20 -0800

    tidyverse refactor of inspect_missing doc update

--- R/inspect_missing.R
+++ R/inspect_missing.R
@@ -1,29 +1,27 @@
-#' @title Inspect missingness
+#' @title Inspect missing values
 #' @description
-#' Quickly inspect the missingness in a dataset.
+#' Quickly inspect the missing values in a dataset by producing a bar plot.
 #'
-#'
-#' @param data A non-empty date frame or tibble.
-#' @param vars A vector of variables contained in data. Default value `NULL` will produce plots for every variable in the data
+#' @param data A non-empty data frame or tibble.
+#' @param vars A character vector of column names to inspect. Default value `NULL` will produce plots for every variable in the data.
 #' @param na_colour An R-supported colour or hex value to indicate missing values. Default value is "red".
 #' @param fill_colour An R-supported colour or hex value to indicate non-missing values. Default value is "grey30".
-#' @param title A string. Default value `NULL` is no title.
-#' @param ... Additional ggplot2 parameters to modify plot outputs.
+#' @param title A non-empty string for the plot title. Default value `NULL` results in no title being displayed.
+#' @param ... Additional `ggplot2` parameters to modify plot outputs (e.g., `alpha`, `colour`).
 #'
-#' @return A ggplot2 object.
+#' @return A ggplot2 object (a bar plot) showing the proportion of missingness for the specified variables.
 #' @details
-#' `inspect_missing` uses ggplot2 to produce a bar plot. Any valid arguments that may be passed to a `geom_bar` layer may also be passed to `inspect_missing` via `...` to modify plot outputs.
+#' `inspect_missing` uses `ggplot2` to produce a bar plot representing the proportion of missing and non-missing values for each column in the dataset. Any valid arguments that may be passed to a `geom_bar` layer in `ggplot2` may also be passed to `inspect_missing` via `...` to customize the plot appearance.
 #'
 #' @export
 #'
 #' @examples
 #' # Basic usage
-#' inspect_missing(iris)
+#' inspect_missing(airquality)
 #'
-#' # Advanced usage
-#' # Manually specifies the variables to inspect and modifies plot output.
-#' inspect_missing(data = iris,
-#'                 vars = c("Sepal.Width", "Sepal.Length"),
+#' # Advanced usage: Manually specify variables and modify plot output
+#' inspect_missing(data = airquality,
+#'                 vars = c("Ozone", "Solar.R"),
 #'                 na_colour = "green",
 #'                 fill_colour = "blue",
 #'                 alpha = 0.5)
@@ -35,12 +33,12 @@ inspect_missing <- function(data,
                             ...) {
   # Check if input is a data frame
   if (!is.data.frame(data)) {
-    stop("Input is not a data frame or a tibble.")
+    rlang::abort("Input is not a data frame or a tibble.")
   }
 
   # Check if the dataset is empty
   if (nrow(data) == 0) {
-    stop("The dataset is empty. No plots will be generated.")
+    rlang::abort("The dataset is empty. No plots will be generated.")
   }
 
   # Use specified variables (if any), otherwise default to all variables
@@ -53,29 +51,32 @@ inspect_missing <- function(data,
   # Check if all specified variables exist in the dataset
   invalid_vars <- setdiff(selected_vars, names(data))
   if (length(invalid_vars) > 0) {
-    stop(paste("The following variables do not exist in the dataset:", paste(invalid_vars, collapse = ", ")))
+    rlang::abort(glue::glue("The following variables do not exist in the dataset: {paste(invalid_vars, collapse = ', ')}"))
   }
 
   # Check if fill colour input is valid
   colour_validate <- function(colour) {
     valid_colors <- grDevices::colors()
-    is_hex_color <- grepl("^#(?:[0-9a-fA-F]{3}){1,2}$", colour)
+    is_hex_color <- stringr::str_detect("^#(?:[0-9a-fA-F]{3}){1,2}$", colour)
+
     if (!(colour %in% valid_colors || is_hex_color)) {
-    stop("Invalid colour: ", colour, ". Please provide a valid colour name or hex code.")
-  }}
+      rlang::abort(glue::glue("Invalid colour: {colour}. Please provide a valid colour name or hex code."))
+    }
+  }
+
   colour_validate(na_colour)
   colour_validate(fill_colour)
 
   # Check if title provided is valid
   if (!is.null(title) && (!is.character(title) || length(title) != 1 || title == "")) {
-    stop("Invalid title: ", title, ". Please provide a non-empty character string.")
+    rlang::abort(glue::glue("Invalid title: {title}. Please provide a non-empty character string."))
   }
 
   # Calculate missing values count for each variable
   missing_values <- data |>
     dplyr::summarise(dplyr::across(dplyr::all_of(selected_vars), ~sum(is.na(.)))) |>
     tidyr::pivot_longer(dplyr::everything(), names_to = "variable", values_to = "missing") |>
-    dplyr::mutate(non_missing = nrow(data) - missing)  # Create non-missing count
+    dplyr::mutate(non_missing = nrow(data) - missing)
 
   # Plot missingness
   plot_missing <- missing_values |>
